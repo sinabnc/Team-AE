193040ab3d5f9d7eaa473b2d36a21b4c256f0e20
sinabnc <sinabnc7@gmail.com>
2024-12-03 12:17:37 +0530

    one

--- Team/web/views.py
+++ Team/web/views.py
@@ -4,20 +4,25 @@ from .models import Blog
 def index(request):
     return render(request,"web/index.html")
 
+
 def about(request):
     return render(request,"web/about.html")
+
 
 def contact(request):
     return render(request,"web/contact.html")
 
+
 def service(request):
     return render(request,"web/services.html")
+
 
 def blog(request):
     blogs = Blog.objects.all()
     context = {"is_blog": True,
                "blogs": blogs}
     return render(request, "web/blog.html", context)
+
 
 def blog_detail(request,slug):
     blog = Blog.objects.get(slug=slug)
@@ -28,20 +33,26 @@ def blog_detail(request,slug):
     
     return render(request, "web/blog-details.html", context)
 
+
 def service_detail(request):
     return render(request,"web/page-service-details.html")
+
 
 def service_detail2(request):
     return render(request,"web/service-details-2.html")
 
+
 def service_detail3(request):
     return render(request,"web/service-details-3.html")
+
 
 def service_detail4(request):
     return render(request,"web/service-details-4.html")
 
+
 def service_detail5(request):
     return render(request,"web/service-details-5.html")
+
 
 def service_detail6(request):
     return render(request,"web/service-details-6.html")
